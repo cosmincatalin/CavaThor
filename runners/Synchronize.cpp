bc4ae0c434ce7a6b589f6029ea6a2940483a7dc1
Cosmin Sanda <cosmincatalin@gmail.com>
2022-11-21 22:35:23 +0100

    s

--- runners/Synchronize.cpp
+++ runners/Synchronize.cpp
@@ -1,27 +1,22 @@
 #define DBG_MACRO_NO_WARNING
 #include <dbg.h>
-#include "hiredis/hiredis.h"
+#include <hiredis/hiredis.h>
+#include <librdkafka/rdkafka.h>
 
 int main()
 {
-    auto *context = redisConnect("127.0.0.1", 6379);
-    if (context == nullptr || context->err) {
-        if (context) {
-            std::cout << (context->errstr) << "\n";
-        } else {
-            dbg("Can't allocate redis context\n");
-        }
-    }
-    auto reply = redisCommand(context, "SET hiredis:03 bar");
-    if (context == nullptr || context->err) {
-        if (context) {
-            std::cout << (context->errstr) << "\n";
-        } else {
-            dbg("Can't allocate redis context\n");
-        }
-    }
-    redisFree(context);
-    dbg(reply);
+    // Kafka Producer
+    std::string_view errstr{""};
+    rd_kafka_conf_t* conf = rd_kafka_conf_new();
+    rd_kafka_t* producer = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
 
+
+    // Redis
+    auto* context = redisConnect("redis", 6379);
+    redisCommand(context, "SET hiredis:04 bar");
+    redisFree(context);
+
+
+    dbg("Done");
     return 0;
 }
