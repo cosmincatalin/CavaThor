b2e20c5738db5baf0fa6969907c1857dff3ae4e7
Cosmin Sanda <cosmincatalin@gmail.com>
2022-11-22 21:46:01 +0100

    sds

--- runners/Synchronize.cpp
+++ runners/Synchronize.cpp
@@ -5,30 +5,47 @@
 
 int main()
 {
-    // Kafka Producer
+    // Kafka Consumer
     char errstr[512];
-    rd_kafka_conf_t* conf = rd_kafka_conf_new();
-    if (rd_kafka_conf_set(conf, "bootstrap.servers", "kafka:9092", errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
-    {
-        dbg(errstr);
-    }
-    rd_kafka_t* producer = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
-    conf = nullptr;
     rd_kafka_resp_err_t err;
-    char const* value = "sadsa";
-    size_t value_len{6};
-    err = rd_kafka_producev(producer,
-                            RD_KAFKA_V_TOPIC("dummy"),
-                            RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
-                            RD_KAFKA_V_VALUE((void*)value, value_len),
-                            RD_KAFKA_V_END);
-    if (err) {
-        dbg("Failed to produce to topic");
-        return 1;
-    } else {
-        dbg("Produced event to topic");
+    rd_kafka_conf_t* conf = rd_kafka_conf_new();
+    rd_kafka_conf_set(conf, "bootstrap.servers", "kafka:9092", errstr, sizeof(errstr));
+    rd_kafka_conf_set(conf, "group.id", "dummy-group-id", errstr, sizeof(errstr));
+    rd_kafka_t* consumer = rd_kafka_new(RD_KAFKA_CONSUMER, conf, errstr, sizeof(errstr));
+    conf = nullptr;
+    const char *topic = "users";
+    rd_kafka_topic_partition_list_t *subscription = rd_kafka_topic_partition_list_new(1);
+    rd_kafka_topic_partition_list_add(subscription, topic, RD_KAFKA_PARTITION_UA);
+    err = rd_kafka_subscribe(consumer, subscription);
+    rd_kafka_topic_partition_list_destroy(subscription);
+    while (true) {
+        rd_kafka_message_t *consumer_message;
+
+        consumer_message = rd_kafka_consumer_poll(consumer, 500);
+        if (!consumer_message) {
+            dbg("Waiting...");
+            continue;
+        }
+
+        if (consumer_message->err) {
+            if (consumer_message->err == RD_KAFKA_RESP_ERR__PARTITION_EOF) {
+                /* We can ignore this error - it just means we've read
+                 * everything and are waiting for more data.
+                 */
+            } else {
+                dbg("Consumer error: %s", rd_kafka_message_errstr(consumer_message));
+                return 1;
+            }
+        } else {
+            dbg(consumer_message->payload);
+        }
+
+        // Free the message when we're done.
+        rd_kafka_message_destroy(consumer_message);
     }
-    rd_kafka_flush(producer, 10 * 1000);
+
+    rd_kafka_consumer_close(consumer);
+    rd_kafka_destroy(consumer);
 
     // Redis
     auto* context = redisConnect("redis", 6379);
