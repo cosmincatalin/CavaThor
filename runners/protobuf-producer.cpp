34f776c28259db81d87128bf39a4d7600e25b981
Cosmin Sanda <cosmincatalin@gmail.com>
2022-11-24 23:14:27 +0100

    d

--- runners/protobuf-producer.cpp
+++ runners/protobuf-producer.cpp
@@ -1,8 +1,15 @@
 #define DBG_MACRO_NO_WARNING
 #include <dbg.h>
+#include "user.pb.h"
 
 int main()
 {
+    runners::User user;
+    *user.mutable_dept() = "DS";
+    *user.mutable_name() = "Catalin";
+    std::string str;
+    user.SerializeToString(&str);
+    dbg(str);
     dbg("Done Proto");
     return 0;
 }
