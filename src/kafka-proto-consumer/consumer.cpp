d31342301ee23ad2b608a3372ce0cf3f6e1ac8c8
Cosmin Sanda <cosmincatalin@gmail.com>
2023-01-21 12:54:45 +0100

    s

--- src/kafka-proto-consumer/consumer.cpp
+++ src/kafka-proto-consumer/consumer.cpp
@@ -1,3 +1,49 @@
-//
-// Created by Cosmin Catalin Sanda on 09/01/2023.
-//
+#define DBG_MACRO_NO_WARNING
+#include "proto/car.pb.h"
+#include <dbg.h>
+#include <librdkafka/rdkafkacpp.h>
+
+static volatile sig_atomic_t run = 1;
+
+static void sigterm(int sig) {
+  run = 0;
+}
+
+int main() {
+
+    signal(SIGINT, sigterm);
+    signal(SIGTERM, sigterm);
+
+    std::string err;
+
+    auto *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+    conf->set("bootstrap.servers", std::getenv("BOOTSTRAP_SERVER"), err);
+    conf->set("group.id", "00002", err);
+
+    auto consumer = RdKafka::KafkaConsumer::create(conf, err);
+
+    std::vector<std::string> topics;
+    topics.push_back("babylon");
+
+    consumer->subscribe(topics);
+
+    while (run) {
+        RdKafka::Message *msg = consumer->consume(1000);
+        if(auto message = msg->payload(); message != nullptr) {
+            proto::Car car;
+            car.ParseFromArray(msg->payload(), msg->len());
+            dbg(car.DebugString());
+            if(msg->key_len() > 0) {
+                dbg(*msg->key());
+            }
+        }
+        delete msg;
+    }
+
+    consumer->close();
+    delete consumer;
+
+    dbg("Done");
+
+    return 0;
+}
